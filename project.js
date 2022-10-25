b16cbe2055b1a2df11cd1d09d3edf421d739aa33
aligocmen <aligocmen@powercoders.org>
2022-10-25 11:08:43 +0200

    some changes

--- project.js
+++ project.js
@@ -2,36 +2,42 @@ let quiz = [
     {
         question:"question1",
         answers:["answer1","answer2","answer3","answer4"],
-        correctAnswer:2
+        correctAnswer:2,
+        shownBefore:false
     },
     {
         question:"question2",
         answers:["answer1","answer2","answer3","answer4"],
-        correctAnswer:3
+        correctAnswer:3,
+        shownBefore:false
     },
     {
         question:"question3",
         answers:["answer1","answer2","answer3"],
-        correctAnswer:2
+        correctAnswer:2,
+        shownBefore:false
     },
     {
         question:"question4",
         answers:["answer1","answer2","answer3","answer4"],
-        correctAnswer:2
+        correctAnswer:2,
+        shownBefore:false
     },
     {
         question:"question5",
         answers:["answer1","answer2","answer3","answer4"],
-        correctAnswer:2
+        correctAnswer:2,
+        shownBefore:false
     }
 ]
 
 const NUMBEROFQUESTIONS = quiz.length;
-let questionIsShown = -1;
+let questionOnTheScreen = -1;
 
 let questionSection = document.getElementById("questions");
 let questionTxt = document.getElementById("questionTxt");
 let answersTxt = document.querySelectorAll("#answers article");
+let answersArticle = document.getElementById("answers");
 
 function generateRandomIndex(){
     let x = Math.floor(Math.random() * NUMBEROFQUESTIONS);
@@ -40,21 +46,33 @@ function generateRandomIndex(){
 
 function showQuestion(){
     // if we get a random number already used we should generate again
-    let x = generateRandomIndex();
-    let randomQuestion = quiz[x];
-    questionIsShown = x;
+    let randomNumber;
+    let randomQuestion;
+    do {
+        randomNumber = generateRandomIndex();
+        randomQuestion = quiz[randomNumber];
+    } while (randomQuestion.shownBefore);
+    
+    randomQuestion.shownBefore = true;
+    questionOnTheScreen = randomNumber;
     questionTxt.innerHTML = randomQuestion.question;
+    let answer;
     for (let i = 0; i < randomQuestion.answers.length; i++) {
-        answersTxt[i].innerHTML = randomQuestion.answers[i];
-        answersTxt[i].addEventListener("click", ()=>{answerClicked(i)});
+        answer = document.createElement("article");
+        answer.innerHTML = randomQuestion.answers[i];
+        answer.addEventListener("click", ()=>{answerClicked(i)});
+        answersArticle.appendChild(answer);
     }
 }
 
 function answerClicked(index){
-    if(quiz[questionIsShown].correctAnswer == index){
+    if(quiz[questionOnTheScreen].correctAnswer == index){
         alert("correct");
     }
     showScore();
+}
+function nextClicked(){
+    showQuestion();
 }
 
 function showScore(){
